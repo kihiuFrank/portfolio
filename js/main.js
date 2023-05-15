cae31dd745efb17411b80662af131bf3fd89ee23
kihiuFrank <franklinekihiu@gmail.com>
2023-05-15 16:31:35 +0300

    about section

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
