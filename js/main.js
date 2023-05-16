3a812c1167faa4e8aad3f908e77840d736400d4f
kihiuFrank <franklinekihiu@gmail.com>
2023-05-16 12:01:26 +0300

    service section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+  serviceModals[modalClick].classList.add("active");
+};
+
+learnMoreBtn.forEach((learnMoreBtn, i) => {
+  learnMoreBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloseBtn.forEach((modalCloseBtn) => {
+  modalCloseBtn.addEventListener("click", () => {
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove("active");
+    });
+  });
+});
