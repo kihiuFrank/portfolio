d6519e0ad0e7e45e2e3bf69f8693a4baff73dd4c
kihiuFrank <franklinekihiu@gmail.com>
2023-05-19 14:45:56 +0300

    clients and get-in-touch sections

--- js/main.js
+++ js/main.js
@@ -49,3 +49,19 @@ projectsCloseBtns.forEach((projectsCloseBtn) => {
     });
   });
 });
+
+// Our Clients - Swiper
+
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
